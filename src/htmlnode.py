b289cc04d8e8b8dbebb146c4292f84ea989361cc
fm-anderson <78233612+fm-anderson@users.noreply.github.com>
2024-05-22 14:45:41 -0400

    feature: LeafNode class and unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,26 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode(tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+        if value is None:
+            raise ValueError("Value is required for LeafNode")
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+
+        if self.tag is None:
+            return self.value
+
+        props_html = self.props_to_html()
+        if props_html:
+            return f"<{self.tag} {props_html}>{self.value}</{self.tag}>"
+        else:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -30,6 +30,35 @@ class TestHTMLNode(unittest.TestCase):
         expected = "HTMLNode(tag: div, value: None, children: [], props: {'class': 'container'})"
         message = "repr should correctly represent the HTMLNode object"
         self.assertEqual(repr(node), expected, message)
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_node_without_value(self):
+        message = "Creating a LeafNode without a value should raise a ValueError"
+        with self.assertRaises(ValueError, msg=message):
+            LeafNode(tag="p", value=None)
+
+    def test_leaf_node_with_no_tag(self):
+        node = LeafNode(tag=None, value="Just text")
+        expected = "Just text"
+        message = "to_html should return raw text when there is no tag"
+        self.assertEqual(node.to_html(), expected, message)
+
+    def test_leaf_node_with_tag_and_no_props(self):
+        node = LeafNode(tag="p", value="This is a paragraph.")
+        expected = "<p>This is a paragraph.</p>"
+        message = (
+            "to_html should render an HTML tag with value when there are no properties"
+        )
+        self.assertEqual(node.to_html(), expected, message)
+
+    def test_leaf_node_with_tag_and_props(self):
+        node = LeafNode(
+            tag="a", value="Click me!", props={"href": "https://www.google.com"}
+        )
+        expected = '<a href="https://www.google.com">Click me!</a>'
+        message = "to_html should render an HTML tag with value and properties"
+        self.assertEqual(node.to_html(), expected, message)
 
 
 if __name__ == "__main__":
