52ada075c84a6b886039a04869f39798177a5a21
fm-anderson <78233612+fm-anderson@users.noreply.github.com>
2024-05-27 18:41:55 -0400

    feature: TextNode class and unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,28 +24,33 @@ class HTMLNode:
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
-        if value is None:
+        if value is None and tag != "img":
             raise ValueError("Value is required for LeafNode")
 
     def to_html(self):
-        if self.value is None:
+        if self.value is None and self.tag != "img":
             raise ValueError("LeafNode must have a value")
 
         if self.tag is None:
             return self.value
 
         props_html = self.props_to_html()
-        if props_html:
-            return f"<{self.tag} {props_html}>{self.value}</{self.tag}>"
+        if self.tag == "img":
+            if props_html:
+                return f"<{self.tag} {props_html}/>"
+            else:
+                return f"<{self.tag}/>"
         else:
-            return f"<{self.tag}>{self.value}</{self.tag}>"
-
-    def __repr__(self):
-        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+            if props_html:
+                return f"<{self.tag} {props_html}>{self.value}</{self.tag}>"
+            else:
+                return f"<{self.tag}>{self.value}</{self.tag}>"
 
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
+        if tag is None:
+            raise ValueError("Tag is required for ParentNode")
         if not children:
             raise ValueError("Children are required for ParentNode")
         super().__init__(tag, None, children, props)
@@ -66,3 +71,38 @@ class ParentNode(HTMLNode):
 
     def __repr__(self):
         return f"ParentNode({self.tag}, {self.children}, {self.props})"
+
+
+class TextNode:
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+    def __init__(self, text_type, text=None, url=None, alt=None):
+        self.text_type = text_type
+        self.text = text
+        self.url = url
+        self.alt = alt
+
+    def text_node_to_html_node(text_node):
+        if text_node.text_type == TextNode.TEXT:
+            return LeafNode(None, text_node.text)
+        elif text_node.text_type == TextNode.BOLD:
+            return LeafNode("b", text_node.text)
+        elif text_node.text_type == TextNode.ITALIC:
+            return LeafNode("i", text_node.text)
+        elif text_node.text_type == TextNode.CODE:
+            return LeafNode("code", text_node.text)
+        elif text_node.text_type == TextNode.LINK:
+            if not text_node.url:
+                raise ValueError("URL is required for link type")
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        elif text_node.text_type == TextNode.IMAGE:
+            if not text_node.url or not text_node.alt:
+                raise ValueError("URL and alt text are required for image type")
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.alt})
+        else:
+            raise ValueError("Unsupported text node type")
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import HTMLNode, LeafNode, ParentNode, TextNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -104,6 +104,52 @@ class TestParentNode(unittest.TestCase):
         expected = "<div><p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p><span>Another child</span></div>"
         message = "to_html should render nested ParentNode objects correctly"
         self.assertEqual(node.to_html(), expected, message)
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text_node_to_html_node_text(self):
+        text_node = TextNode(TextNode.TEXT, text="Just text")
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "Just text")
+
+    def test_text_node_to_html_node_bold(self):
+        text_node = TextNode(TextNode.BOLD, text="Bold text")
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<b>Bold text</b>")
+
+    def test_text_node_to_html_node_italic(self):
+        text_node = TextNode(TextNode.ITALIC, text="Italic text")
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<i>Italic text</i>")
+
+    def test_text_node_to_html_node_code(self):
+        text_node = TextNode(TextNode.CODE, text="Code text")
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<code>Code text</code>")
+
+    def test_text_node_to_html_node_link(self):
+        text_node = TextNode(
+            TextNode.LINK, text="Click here", url="https://example.com"
+        )
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(
+            html_node.to_html(), '<a href="https://example.com">Click here</a>'
+        )
+
+    def test_text_node_to_html_node_image(self):
+        text_node = TextNode(
+            TextNode.IMAGE, url="https://example.com/image.png", alt="An image"
+        )
+        html_node = TextNode.text_node_to_html_node(text_node)
+        self.assertEqual(
+            html_node.to_html(),
+            '<img src="https://example.com/image.png" alt="An image"/>',
+        )
+
+    def test_text_node_to_html_node_unsupported_type(self):
+        text_node = TextNode("unsupported", text="Unsupported text")
+        with self.assertRaises(ValueError):
+            TextNode.text_node_to_html_node(text_node)
 
 
 if __name__ == "__main__":
